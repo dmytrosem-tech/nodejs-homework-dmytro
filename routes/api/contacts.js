3b8502d6356a20cb5bb7fb3c30ab4dc69c4fd7f4
Dmytro Semeruk <deemoon83@gmail.com>
2021-12-20 17:25:52 +0200

    20-h

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import model from '../../model/index'
+import { validateCreate, validateUpdate, validateId } from './validation'
 const router = new Router()
 
 router.get('/', async (req, res, next) => {
@@ -7,7 +8,7 @@ router.get('/', async (req, res, next) => {
   res.status(200).json(contacts)
 })
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validateId, async (req, res, next) => {
   const { id } = req.params
   const contact = await model.getContactById(id)
   if (contact) {
@@ -17,12 +18,12 @@ router.get('/:id', async (req, res, next) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', validateCreate, async (req, res, next) => {
   const newContact = await model.addContact(req.body)
   res.status(201).json(newContact)
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', validateId, async (req, res, next) => {
   const { id } = req.params
 
   const contact = await model.removeContact(id)
@@ -33,7 +34,7 @@ router.delete('/:id', async (req, res, next) => {
   res.status(404).json({ message: 'not found' })
 })
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', validateId, validateUpdate, async (req, res, next) => {
   const { id } = req.params
 
   const contact = await model.updateContact(id, req.body)
