66b95c9760be934345bb34895abbada61879838d
Dmytro Semeruk <deemoon83@gmail.com>
2021-12-24 15:53:20 +0200

    24-w2

--- model/index.js
+++ model/index.js
@@ -1,67 +1,19 @@
-import fs from 'fs/promises'
-import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
-import contacts from './contacts.json'
-import { fileURLToPath } from 'url'
+import db from './db'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-const listContacts = () => {
-  return contacts
+const listContacts = async () => {
+  const client = await db
+  const collection = await client.db().collection('contacts')
+  // const result
+  return collection
 }
 
-const getContactById = (contactId) => {
-  const contact = contacts.find((contact) => contact.id === contactId)
-  return contact
-}
+const getContactById = (contactId) => {}
 
-const removeContact = async (contactId) => {
-  const index = contacts.findIndex((contact) => contact.id === contactId)
-  if (index !== -1) {
-    const [result] = contacts.splice(index, 1)
-    await fs.writeFile(
-      path.join(__dirname, 'contacts.json'),
-      JSON.stringify(contacts, null, 2)
-    )
-    return result
-  }
-  return null
-}
+const removeContact = async (contactId) => {}
 
-/* вариант через перебор и some */
-// if (contacts.some((contact) => contact.id === contactId)) {
-//   const newContacts = contacts.filter((contact) => contact.id !== contactId)
-//   await fs.writeFile(
-//     path.join(__dirname, 'db', 'contacts.json'),
-//     JSON.stringify(newContacts, null, 2)
-//   )
-//   return newContacts
-// }
-// return contacts
+const addContact = async ({ name, email, phone }) => {}
 
-const addContact = async ({ name, email, phone }) => {
-  const newContact = { name, email, phone, id: uuidv4() }
-  contacts.push(newContact)
-  await fs.writeFile(
-    path.join(__dirname, 'contacts.json'),
-    JSON.stringify(contacts, null, 2)
-  )
-  return newContact
-}
-
-const updateContact = async (contactId, body) => {
-  const index = contacts.findIndex((contact) => contact.id === contactId)
-  if (index !== -1) {
-    const updatedContact = { id: contactId, ...contacts[index], ...body }
-    contacts[index] = updatedContact
-    await fs.writeFile(
-      path.join(__dirname, 'contacts.json'),
-      JSON.stringify(contacts, null, 2)
-    )
-    return updatedContact
-  }
-  return null
-}
+const updateContact = async (contactId, body) => {}
 
 export default {
   listContacts,
